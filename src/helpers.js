266607a9b1876a90b59b2e4ff31b8761129f653d
Maria Gabriela Barrios Cantera <barrios.cantera@gmail.com>
2020-02-04 14:25:07 +1100

    demo exercise

---
+++ src/helpers.js
@@ -0,0 +1,14 @@
+function helpful() {
+  console.log("I DID A SUPER HELPFUL THING");
+}
+
+function sing() {
+  console.log("LA LA LA");
+}
+
+function sort() {
+  console.log("ALL SORTED!");
+}
+
+export default helpful;
+export {sing, sort};
